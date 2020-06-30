42c426416cde222c8e40fad6c0a5a7c5ed76efd8
malakkhashoqji <malakkhashogji2550@gmail.com>
2020-06-30 14:49:04 +0300

    updated

--- ArduinoIDE/Flutter_LED.ino
+++ ArduinoIDE/Flutter_LED.ino
@@ -11,11 +11,11 @@ boolean ledState = false;
 ESP8266WebServer server(80);
 void handleRoot() {
     // try typing your ip adress in your browser to check your connection
-  server.send(200, "text/html", "<h1>Device is Connected</h1>");
+  server.send(200, "text/html", "<h1>The connection is sucessful</h1>");
 }
 void setup() {
   // put your setup code here, to run once:
-  pinMode(14, OUTPUT);// GPIO14 
+  pinMode(14, OUTPUT);// GPIO14
   Serial.begin(9600);
   //Wi-fi connection
   WiFi.begin(ssid, password);
@@ -25,7 +25,7 @@ void setup() {
   }
 
   // change it your ip adress
-  
+
   IPAddress ip(192, 168, 1, 40);
    // if your are using mac OS: use this command in your terminal to get the ip adress: ipconfig getifaddr en0
   // if ypur are using windows OS: use this command in your cmd to get the ip adress: ipconfig
@@ -33,7 +33,7 @@ void setup() {
   IPAddress gateway(192, 168, 1, 1);
   IPAddress subnet(255, 255, 255, 0);
   WiFi.config(ip, gateway, subnet);
- 
+
 
 
   server.on("/", handleRoot); // if you type your IP adress in your browser the handleRoot function will be called, and the connection massage will be displayed in your browser
@@ -43,26 +43,22 @@ void setup() {
   Serial.println("Http has Started");
 
 }
-
-
-
-
 void turnOnLed() {
   ledState = true;
   digitalWrite(14, ledState);
   server.send(200, "text/plain", "off");
-  
+
 }
 
 void turnOffLed() {
   ledState = false;
   digitalWrite(14, ledState);
   server.send(200, "text/plain", "on");
- 
+
 }
 //
 void loop() {
   // put your main code here, to run repeatedly:
   server.handleClient();
-  // let the server handle data coming from the client 
+  // let the server handle data coming from the client
 }
